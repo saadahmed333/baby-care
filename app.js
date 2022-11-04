08ecfc1ab29db503fbc48eff70bb5783ef4a98db
saad <saadahmed1742@gmail.com>
2022-11-04 16:08:17 +0500

    front page gifts and explore structure and page change

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
 let toggle = document.getElementById("toggle");
-let sideBar = document.getElementById("side-bar"); 
+let sideBar = document.getElementById("side-bar");
 let cross = document.getElementById("cross");
 
 toggle.addEventListener("click", () => {
-    sideBar.style.left = "0px"
-})
+  sideBar.style.left = "0px";
+});
 cross.addEventListener("click", () => {
-    sideBar.style.left = "-700px"
-})
+  sideBar.style.left = "-700px";
+});
